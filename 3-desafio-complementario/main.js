d016702594bc7bc449543d7f4404a1875ee35ecb
rooramirez <roomeli.ramirez@gmail.com>
2022-09-20 22:06:46 -0300

    se finaliza el desafio complementario, sin errores

--- 3-desafio-complementario/main.js
+++ 3-desafio-complementario/main.js
@@ -59,7 +59,10 @@ const verListaTarea = () => {
     //-----------DOM--------
 
     let container = document.getElementById('tareas');
+    let divContainer = document.getElementById('divDeTareas');
 
+    divContainer.innerHTML = '';
+     
     arrayTareas.forEach( tarea => {
         let divTarea = document.createElement('div');
         divTarea.classList.add('elementoLista');
@@ -70,9 +73,10 @@ const verListaTarea = () => {
         <p>Frecuencia  de la tarea: ${tarea.frecuencia}.</p>
         <p>Responsable  de la tarea: ${tarea.responsable}.</p>
         `
-    container.appendChild(divTarea);
-});
 
+    divContainer.appendChild(divTarea);
+});
+    container.appendChild(divTarea);
 //-------------fin del DOM-------
     console.log(arrayTareas);
 }
@@ -104,6 +108,9 @@ const agregarElementoAListaCompras = () => {
 const verListaCompras = () => {
     //-----------DOM-------
     let container = document.getElementById('compras');
+    let divContainer = document.getElementById('divDeCompras');
+
+    divContainer.innerHTML = '';
 
     arrayCompras.forEach( compra => {
         let divCompra = document.createElement('div');
@@ -112,8 +119,9 @@ const verListaCompras = () => {
         <p>Producto: ${compra.producto}</p>
         <p>Tipo de producto: ${compra.tipo}</p>
         `
-        container.appendChild(divCompra);
+        divContainer.appendChild(divCompra)
     });
+    container.appendChild(divCompra);
     //---------fin DOM ------
     console.log(arrayCompras);
 }
@@ -133,63 +141,74 @@ const modificarListaCompras = () => {
     console.log(arrayCompras);
 }
 ////////fin de la funcion de orden superior///////////
-//inicio de la aplicacion:
-let bienvenida = console.log('Bienvenido a la aplicacion web "Armonía Familiar", destinado a la organizacion y division de tareas equitativamente.');
 
 
-while (ingresoALaAplicacion != 0) {
-    ingresoALaAplicacion = Number(prompt('¿Que desea hacer? Ingrese: \n 1 para agregar una tarea, \n 2 para ver lista de tareas, \n 3 para modificar la lista de tareas, \n 4 para agregar un elemento a la lista de compras, \n 5 para ver la lista de compras, \n 6 para modificar la lista de compras \n o ingrese 0 (cero) para finalizar el programa.'));
+///---------inicio de funcion para ingresar a la aplicacion---------------
+
+const iniciarAplicacion = () => {
+
+    let bienvenida = console.log('Bienvenido a la aplicacion web "Armonía Familiar", destinado a la organizacion y division de tareas equitativamente.');
     
-    switch (ingresoALaAplicacion) {
-        case 1:
-            agregarTarea();
-            console.log(`Se agrego la nueva tarea con exito! ${arrayTareas[arrayTareas.length-1].verTarea()}`);
-            break;
-        case 2:
-            if (arrayTareas.length === 0) {
-                console.log('aun no hay ninguna tarea en la lista, se encuentra vacía');
+    while (ingresoALaAplicacion != 0) {
+        
+        ingresoALaAplicacion = Number(prompt('¿Que desea hacer? Ingrese: \n 1 para agregar una tarea, \n 2 para ver lista de tareas, \n 3 para modificar la lista de tareas, \n 4 para agregar un elemento a la lista de compras, \n 5 para ver la lista de compras, \n 6 para modificar la lista de compras \n o ingrese 0 (cero) para finalizar el programa.'));
+        
+        switch (ingresoALaAplicacion) {
+            case 1:
+                agregarTarea();
+                console.log(`Se agrego la nueva tarea con exito! ${arrayTareas[arrayTareas.length-1].verTarea()}`);
+                break;
+            case 2:
+                if (arrayTareas.length === 0) {
+                    console.log('aun no hay ninguna tarea en la lista, se encuentra vacía');
+                }
+                else{
+                    verListaTarea();
+                }
+                break;
+            case 3:
+                if (arrayTareas.length === 0) {
+                    console.log('aun no hay ninguna tarea en la lista, se encuentra vacía');
+                }
+                else{
+                    eliminarTarea();
+                    alert('la tarea se borro con exito y se agrego a la lista de tareas realizadas');
+                    console.log(`tareas realizadas: `);
+                    console.log(arrayTareasCompletadas);
+                } 
+                break;
+            case 4:
+                agregarElementoAListaCompras();
+                console.log(`Se agrego el elemento a la lista de compras con exito! ${arrayCompras[arrayCompras.length-1].verListaCompras()}`);
+                break;
+            case 5:
+                if (arrayCompras.length === 0) {
+                    console.log('aun no hay ningun elemento en la lista de Compras, se encuentra vacía');
+                }
+                else{
+                    verListaCompras();
+                }
+                break;
+            case 6:
+                if (arrayCompras.length === 0) {
+                    console.log('aun no hay ningun elemento en la lista de Compras, se encuentra vacía');
+                }
+                else{
+                    modificarListaCompras();
+                    alert('el producto se borro con exito y se agrego a la lista de productos eliminados');
+                    console.log(`productos eliminados: `);
+                    console.log(arrayProductosEliminados);
+                }
+                break;
+            default:
+                console.log('La opcion ingresada es incorrecta, repitala nuevamente');
+                break;
             }
-            else{
-                verListaTarea();
-            }
-            break;
-        case 3:
-            if (arrayTareas.length === 0) {
-                console.log('aun no hay ninguna tarea en la lista, se encuentra vacía');
-            }
-            else{
-                eliminarTarea();
-                alert('la tarea se borro con exito y se agrego a la lista de tareas realizadas');
-                console.log(`tareas realizadas: `);
-                console.log(arrayTareasCompletadas);
-            } 
-            break;
-        case 4:
-            agregarElementoAListaCompras();
-            console.log(`Se agrego el elemento a la lista de compras con exito! ${arrayCompras[arrayCompras.length-1].verListaCompras()}`);
-            break;
-        case 5:
-            if (arrayCompras.length === 0) {
-                console.log('aun no hay ningun elemento en la lista de Compras, se encuentra vacía');
-            }
-            else{
-                verListaCompras();
-            }
-            break;
-        case 6:
-            if (arrayCompras.length === 0) {
-                console.log('aun no hay ningun elemento en la lista de Compras, se encuentra vacía');
-            }
-            else{
-                modificarListaCompras();
-                alert('el producto se borro con exito y se agrego a la lista de productos eliminados');
-                console.log(`productos eliminados: `);
-                console.log(arrayProductosEliminados);
-            }
-            break;
-        default:
-            console.log('La opcion ingresada es incorrecta, repitala nuevamente');
-            break;
-        }
-
+    
+    }
 }
+
+///------fin de funcion de ingreso a la aplicacion------
+
+//---------se llama a la funcion  iniciarAplicacion------------
+iniciarAplicacion();
